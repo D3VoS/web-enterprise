f8708a75bd21aa4a59a785dddedece684c1d712d
=D3V <davidardow@gmail.com>
2022-04-04 07:37:02 +0100

    Added EJS, started basic routing
    Added EJS for frontend templating, started using routers at a basic level

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+const port = PORT;
+const DBURI = process.env.DBURI || "mongodb://127.0.0.1:27017";
+const MongoClient = require('mongodb').MongoClient;
+const path = require('path')
 
-app.get('/', (req, res) =>{
-	res.send("hello world")
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.route('/').get(function(req,res){
+  res.render('index')
 })
 
-app.listen(port, () => {
-	console.log(`Listening on prot ${port}`)
-})
+app.route('/login').get(function(req,res){
+  res.render('login')
+});
+
+
+app.listen(PORT);
+console.log('Express Server running on port '.PORT);
